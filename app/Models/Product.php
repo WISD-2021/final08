e60e36bad263a346a6ae76f326b897f22f43e07f
3A832094 <3a832094@gm.student.ncut.edu.tw>
2021-12-15 11:59:00 +0800

    建立cart_items和products的關係(1:N)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Cart_item;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+    public function cart_items()
+    {
+        return $this->belongsTo(Cart_item::class);
+    }
 }
