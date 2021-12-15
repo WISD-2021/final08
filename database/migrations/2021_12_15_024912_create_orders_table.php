d312fd6d0bc3150fdc01fc0d25c795f7a2cbd283
LIN GUAN-YU <kib7410@gmail.com>
2021-12-15 11:16:31 +0800

    '建立Order的model、migration、controller、以及factory'

---
+++ database/migrations/2021_12_15_024912_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id')->length(11);
+            $table->integer('cart_item_id')->length(11);
+            $table->integer('status')->length(11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
