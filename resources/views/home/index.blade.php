51c0b3d3691da58d1ac4f57290b2229bcba6061d
LIN GUAN-YU <kib7410@gmail.com>
2022-01-07 15:09:26 +0800

    '增加首頁的css樣式'

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,63 @@
 
 @section('title','首頁')
 
+<head>
+    <meta charset="UTF-8">
+    <!--以IE瀏覽器相容模式來顯示，預設為微軟最新的edge瀏覽器模式來顯示網頁-->
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <!--網頁寬度設定為行動裝置的螢幕寬度且縮放比例為1:1-->
+    <meta name="viewport" content="width-device-width, initial-scale-1">
+    <title>首頁</title>
+    <!--Bootstrap 核心 CSS 檔案-->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!--Bootstrap 佈景主題 CSS 檔案-->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <!--jquery 的核心檔案-->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <!--Bootstrap 核心 js 檔案-->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        /*顯示卡圖片樣式*/
+        .img01{
+            top:620px;
+            left:200px;
+            position:absolute;
+        }
+        /*記憶體圖片樣式*/
+        .img02{
+            top:620px;
+            left:600px;
+            position:absolute;
+        }
+        /*CPU圖片樣式*/
+        .img03{
+            top:620px;
+            left:1000px;
+            position:absolute;
+        }
+        /*文字名稱樣式*/
+        .text01{
+            font-family:Helvetica;
+            font-size:18px;
+            font-weight:bolder;
+            text-align:center;
+        }
+        /*底部樣式*/
+        html, body {
+            height:100%;
+        }
+        .wrap {
+            min-height:100%;
+            *display:table; /* For IE7 Hack */
+            *height:100%; /* For IE7 Hack */
+        }
+        .main {
+            overflow:auto;
+            padding-bottom:180px;
+        } /* must be same height as the footer */
+    </style>
+</head>
+
 @section('content')
 
     <!--div[class="carousel slide" id="myCarousel"]-->
