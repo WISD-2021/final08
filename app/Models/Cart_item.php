80e8c6ab4a76c91d0655150f60bfc4ebc2996507
LIN GUAN-YU <kib7410@gmail.com>
2022-01-09 18:13:17 +0800

    '重新命名cart_item資料庫裡面的customer_id為user_id'

--- app/Models/Cart_item.php
+++ app/Models/Cart_item.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart_item extends Model
 {
     use HasFactory;
+
     public function products()
     {
         return $this->hasMany(Product::class);
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -17,4 +17,5 @@ class Order extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
 }
